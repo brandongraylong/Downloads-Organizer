5157072ca37fb303e6d4964382b2dd1213c5a391
Brandon Long <brandon@orgplaza.com>
2019-10-09 22:04:43 -0500

    Changed python3 to python

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source venv/bin/activate
-python3 /home/brandon/Documents/Github\ -\ brandongraylong/Downloads-Organizer/main.py
+python /home/brandon/Documents/Github\ -\ brandongraylong/Downloads-Organizer/main.py
 deactivate
